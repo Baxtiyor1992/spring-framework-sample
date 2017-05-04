fc376981b0fd8f17fbd005d7d84eff7f00b3d732
Bakhtiyorbek Begmatov <baxtiyorbek1992@gmail.com>
2017-05-04 17:04:57 +0500

    Enabling java based configuration

--- src/main/java/com/bakhtiyor/sample/Application.java
+++ src/main/java/com/bakhtiyor/sample/Application.java
@@ -6,11 +6,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext();
-		// MessageRenderer mr = context.getBean(MessageRenderer.class);
-		// MessageProvider mp = context.getBean(MessageProvider.class);
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		MessageRenderer mr = context.getBean(MessageRenderer.class);
+		MessageProvider mp = context.getBean(MessageProvider.class);
 
-		// mr.setMessageProvider(mp);
-		// mr.render();
+		mr.setMessageProvider(mp);
+		mr.render();
 	}
 }
